109c8ccc5dd2fb636f66ab8cb8d0f745ed46be9f
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2013-07-30 17:20:25 +0800

    move cookbooks to github/kuansim/.

--- cookbooks/kuansim/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'kuansim'
-maintainer       'Chen Hsin-Yi (hychen)'
-maintainer_email 'ossug.hychen@gmail.com'
-license          'BSD'
-description      'Installs/Configures kuansim'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
