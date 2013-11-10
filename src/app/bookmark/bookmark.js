9b7a9b321c1bf5b07f0dc26c6bd69f9857123e39
Jason.S.Chen <jasonschen5@gmail.com>
2013-11-10 12:17:33 -0800

    added create bookmark page

--- src/app/bookmark/bookmark.js
+++ src/app/bookmark/bookmark.js
@@ -1,11 +1,15 @@
 angular.module('kuansim.bookmark', [
-  'ui.router'
+  'ui.router',
+  'ui.bootstrap'
 ])
 
 .factory('Bookmark', function($http) {
   return {
     getBookmarks: function() {
       return $http.get('/collections/bookmarks/');
+    },
+    createBookmark: function(bookmark) {
+      return $http.post('/collections/bookmarks/', bookmark);
     }
   };
 })
@@ -13,6 +17,10 @@ angular.module('kuansim.bookmark', [
 .controller('BookmarkCtrl', function BookmarkCtrl($scope, Bookmark) {
 
   $scope.bookmarks = [];
+
+  $rootScope.$watch('notification', function() {
+    $scope.notification = $rootScope.notification;
+  });
 
   var getAllBookmarks = {
     success: function(data, status) {
@@ -23,6 +31,61 @@ angular.module('kuansim.bookmark', [
     }
   };
   // Bookmark.getBookmarks().success(getAllBookmarks.success).error(getAllBookmarks.error);
+
+})
+
+.controller('BookmarkCreateCtrl', function BookmarkCreateCtrl($scope, $timeout, $rootScope, Bookmark) {
+
+  $scope.dateOptions = {
+    'year-format': "'yy'",
+    'starting-day': 1
+  };
+
+  $scope.open = function() {
+    $timeout(function() {
+      $scope.opened = true;
+    });
+  };
+
+  $scope.today = function() {
+    $scope.dateToday = new Date();
+  };
+
+  $scope.disabled = function(date, mode) {
+    return (mode === 'day' && (date > $scope.dateToday));
+  };
+
+  var callbacks = {
+    success: function(data, status) {
+      $rootScope.notification = {
+        status: "success",
+        message: "Successfully created bookmark."
+      };
+      $rootScope.$apply();
+    },
+    error: function(data, status) {
+      $rootScope.notification = {
+        status: "error",
+        message: "Failed to create bookmark."
+      };
+      $rootScope.$apply();
+    }
+  };
+
+  $rootScope.$watch('notification', function() {
+    $scope.notification = $rootScope.notification;
+  });
+
+  $scope.createEvent = function() {
+    var bookmark = {
+      title: $scope.bookmarkTitle,
+      dateHappened: $scope.bookmarkDate.UTCgetMilliseconds,
+      location: $scope.bookmarkLocation,
+      description: $scope.bookmarkDescription
+    };
+    Bookmark.createBookmark(bookmark).success(callbacks.success).error(callbacks.error);
+  };
+
 })
 
 .config(function ($stateProvider) {
@@ -32,6 +95,12 @@ angular.module('kuansim.bookmark', [
       title: 'Bookmarks',
       templateUrl: 'bookmark/bookmark.tpl.html',
       controller: 'BookmarkCtrl'
+    })
+    .state('bookmarkCreate', {
+      url: '/bookmark/create',
+      title: 'Bookmarks',
+      templateUrl: 'bookmark/bookmark_create.tpl.html',
+      controller: 'BookmarkCreateCtrl'
     })
     ;
 })
