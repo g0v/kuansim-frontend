61656090a53b8c6333762e5392807d7c2f7b516d
Jerry Chung <jerry.chung92@gmail.com>
2013-11-09 20:07:57 -0800

    modified gruntfile, login works now

--- src/app/nav/nav.js
+++ src/app/nav/nav.js
@@ -2,13 +2,27 @@ angular.module('kuansim.nav', [
   'kuansim.user'
 ])
 
-.controller('NavCtrl', function NavCtrl($scope, OAuth) {
-  $scope.testOAuth = function () {
-    OAuth.popup('google', function(error, result) {
+.controller('NavCtrl', function NavCtrl($scope, OAuth, $http) {
 
+  $scope.thirdPartySignIn = function(provider) {
+    OAuth.popup(provider, function(error, result) {
+      if (error) {
+        console.log(error);
+      } else {
+        $http.post('/users/authenticate', {
+          provider: provider,
+          access: result.access_token
+        }).
+          success(function(data) {
+            console.log(data);
+          }).
+          error(function(data) {
+            console.log(data);
+          });
+      }
     });
   };
-})
 
-;
+});
+
 
