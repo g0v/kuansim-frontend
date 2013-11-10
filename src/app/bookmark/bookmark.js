f9cd6f515723a09726faa13b54b4fd05c3060161
Jason.S.Chen <jasonschen5@gmail.com>
2013-11-09 21:46:29 -0800

    initial implementation of bookmark.js (factory, controller); still waiting for backend api implementation, so its still untested

--- src/app/bookmark/bookmark.js
+++ src/app/bookmark/bookmark.js
@@ -1,7 +1,26 @@
 angular.module('kuansim.bookmark', [
 ])
 
-.controller('BookmarkCtrl', function BookmarkCtrl($scope) {
+.factory('Bookmark', function($http) {
+  return {
+    getBookmarks: function() {
+      return $http.get('/collections/bookmarks/');
+    },
+  };
+})
+
+.controller('BookmarkCtrl', function BookmarkCtrl($scope, Bookmark) {
+
+  var getAllBookmarks = {
+    success: function(data, status) {
+      $scope.bookmarks = data.events;
+    },
+    error: function(data, status) {
+      $scope.bookmarks = [];
+    }
+  };
+  Bookmark.getBookmarks().success(getAllBookmarks.success).error(getAllBookmarks.error);
+
 })
 
 ;
