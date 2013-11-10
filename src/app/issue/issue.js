51e5e061a2d1022be905f7f21d1d08820222e404
Rae Liu <happyaray@gmail.com>
2013-11-09 22:50:49 -0800

    add timelinejs file/issue

---
+++ src/app/issue/issue.js
@@ -0,0 +1,15 @@
+angular.module('kuansim.issue', [
+
+])
+
+.directive('timeline', function ($timeout) {
+  return {
+    restrict: 'A',
+    scope: {},
+    link: function (scope, element, attrs) {
+
+    }
+  };
+})
+
+;
